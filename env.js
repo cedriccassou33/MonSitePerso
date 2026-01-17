cdd1007722051db7f0dc9e84c2a3da01590101a3
cedriccassou33 <cedric.cassou33@gmail.com>
2026-01-17 16:16:31 +0100

    ménage

--- env.js
+++
@@ -1,9 +0,0 @@
-<script>
-  // Vercel n’injecte pas directement les variables dans un site statique.
-  // On utilise la réécriture côté build via "Build Command" ou on colle les valeurs au moment du déploiement.
-  // Méthode simple sous Vercel : on crée env.js au déploiement avec les valeurs.
-  window.__ENV__ = {
-    SUPABASE_URL: "%%SUPABASE_URL%%",
-    SUPABASE_ANON_KEY: "%%SUPABASE_ANON_KEY%%"
-  };
-</script>
